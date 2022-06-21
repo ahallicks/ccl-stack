7782747e39f565ab53d3848bd681f875cacb585e
Alex Hall <alex.hall@codecomputerlove.com>
2022-06-21 11:00:07 +0100

    chore: upgrade from upstream changes

--- cypress/support/delete-user.ts
+++ cypress/support/delete-user.ts
@@ -3,20 +3,17 @@
 // npx ts-node --require tsconfig-paths/register ./cypress/support/delete-user.ts username@example.com
 // and that user will get deleted
 
-import { installGlobals } from "@remix-run/node/globals";
-import { prisma } from "~/db.server";
+import { installGlobals } from '@remix-run/node/globals';
 
 installGlobals();
 
 async function deleteUser(email: string) {
-  if (!email) {
-    throw new Error("email required for login");
-  }
-  if (!email.endsWith("@example.com")) {
-    throw new Error("All test emails must end in @example.com");
-  }
-
-  await prisma.user.delete({ where: { email } });
+	if (!email) {
+		throw new Error('email required for login');
+	}
+	if (!email.endsWith('@example.com')) {
+		throw new Error('All test emails must end in @example.com');
+	}
 }
 
 deleteUser(process.argv[2]);
--- remix.config.js
+++ remix.config.js
@@ -2,13 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-	ignoredRouteFiles: ['.*'],
 	serverBuildDirectory: 'server/build',
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
 	// serverBuildPath: "build/index.js",
 	// publicPath: "/build/",
 	// devServerPort: 8002
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+	cacheDirectory: './node_modules/.cache/remix',
+	ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 };
